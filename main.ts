ae4276ec6a3ae1c46edfd85d4a266d484e0b75f2
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-01-27 10:59:13 -0500

    Restore '1/23/2025, 1:22:24 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -59,13 +59,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, l
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     game.gameOver(false)
 })
-controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
-    for (let value of tiles.getTilesByType(assets.tile`SpawnChaseCar`)) {
-        tiles.setTileAt(value, assets.tile`myTile13`)
-        CopCar.follow(ChaseCar, 60)
-        info.setScore(1)
-    }
-})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
     tiles.setTileAt(location, assets.tile`myTile9`)
     scene.cameraShake(2, 500)
@@ -107,9 +100,6 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     500,
     true
     )
-})
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprite.setBounceOnWall(true)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.skillmap.islandTile3, function (sprite, location) {
     tiles.setTileAt(location, assets.tile`myTile9`)
@@ -167,7 +157,7 @@ function PullOutCopCar () {
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Enemy)
         tiles.placeOnTile(CopCar, value)
-        CopCar.follow(ChaseCar, 60)
+        CopCar.follow(ChaseCar, 50)
     }
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -199,9 +189,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, l
     tiles.setTileAt(location, assets.tile`myTile9`)
     scene.cameraShake(2, 500)
 })
-function CopCarAnimationChange () {
-	
-}
 scene.onOverlapTile(SpriteKind.Player, sprites.skillmap.islandTile1, function (sprite, location) {
     tiles.setTileAt(location, assets.tile`myTile9`)
     scene.cameraShake(2, 500)
@@ -231,6 +218,7 @@ function SetCopCar () {
             . . . . f f . . . . . . f f . . 
             `, SpriteKind.Enemy)
         tiles.placeOnTile(CopCar, value)
+        CopCar.follow(ChaseCar, 50)
     }
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.skillmap.islandTile4, function (sprite, location) {
@@ -242,62 +230,3 @@ let ChaseCar: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
 SetChaseCar()
 SetCopCar()
-game.onUpdate(function () {
-	
-})
-game.onUpdate(function () {
-    if (CopCar.vy > 0) {
-        animation.runImageAnimation(
-        CopCar,
-        [img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . 6 6 6 6 6 6 . . . . 
-            . . . . . 6 6 9 9 6 6 6 6 . . . 
-            . . . . . c 9 6 6 6 6 6 c . . . 
-            . . . . 6 c 9 6 6 6 6 6 c 6 . . 
-            . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
-            . . . f 6 c 9 6 6 6 6 6 c 6 f . 
-            . . . f 8 c 6 6 6 6 6 6 c 8 f . 
-            . . . f 6 c 6 b b b b 6 c 6 f . 
-            . . . 8 6 6 b c c c c b 6 6 8 . 
-            . . . 8 8 b c c c c c c b 8 8 . 
-            . . . f 8 9 9 9 9 9 9 9 9 8 f . 
-            . . . f 8 d 6 6 6 6 6 6 d 8 f . 
-            . . . . 6 d d 6 6 6 6 d d 6 f . 
-            . . . . f 6 d 6 6 6 6 d 6 f . . 
-            . . . . . 8 6 6 6 6 6 6 8 . . . 
-            `],
-        500,
-        true
-        )
-    } else if (CopCar.vy < 0) {
-        animation.runImageAnimation(
-        CopCar,
-        [img`
-            . . . . . . 8 8 c c 8 8 . . . . 
-            . . . . . 8 6 6 6 6 6 6 8 . . . 
-            . . . . 6 c 6 6 6 6 6 6 c 6 . . 
-            . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
-            . . . f 6 6 9 6 6 6 6 6 c 6 f . 
-            . . . f 6 6 9 6 6 6 6 6 6 6 f . 
-            . . . f 6 6 9 6 6 6 6 6 6 6 f . 
-            . . . f 6 c 6 9 9 6 6 6 c 6 f . 
-            . . . 8 6 c 8 c c c c 8 c 6 8 . 
-            . . . 8 6 8 c b b b b c 8 6 8 . 
-            . . . 8 6 8 b b b b b b 8 6 8 . 
-            . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
-            . . . f 8 d 8 8 8 8 8 8 d 8 f . 
-            . . . f 8 6 d 8 8 8 8 d 6 8 f . 
-            . . . f f 8 8 8 8 8 8 8 8 f f . 
-            . . . . f f . . . . . . f f . . 
-            `],
-        500,
-        true
-        )
-    }
-})
-game.onUpdateInterval(1000, function () {
-    if (controller.anyButton.isPressed()) {
-        info.changeScoreBy(1)
-    }
-})
