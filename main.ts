9007c198ea2eb073542e086321a706bd2706726b
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-02-03 13:29:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -38,10 +38,6 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.OverlapChangeLevel, function (sprite, otherSprite) {
-    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
-    sprites.destroyAllSpritesOfKind(SpriteKind.OverlapChangeLevel)
-})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     game.gameOver(false)
 })
@@ -153,9 +149,6 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         )
     }
 })
-scene.onOverlapTile(SpriteKind.Enemy, sprites.builtin.brick, function (sprite, location) {
-	
-})
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
 	
 })
@@ -187,6 +180,98 @@ function PullOutCopCar () {
         tiles.placeOnTile(CopCar, value)
         CopCar.follow(MainCar, 50)
         tiles.setTileAt(value, sprites.vehicle.roadHorizontal)
+        characterAnimations.loopFrames(
+        CopCar,
+        [img`
+            . . . . . . 8 8 c c 8 8 . . . . 
+            . . . . . 8 6 6 6 6 6 6 8 . . . 
+            . . . . 6 c 6 6 6 6 6 6 c 6 . . 
+            . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
+            . . . f 6 6 9 6 6 6 6 6 c 6 f . 
+            . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+            . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+            . . . f 6 c 6 9 9 6 6 6 c 6 f . 
+            . . . 8 6 c 8 c c c c 8 c 6 8 . 
+            . . . 8 6 8 c b b b b c 8 6 8 . 
+            . . . 8 6 8 b b b b b b 8 6 8 . 
+            . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
+            . . . f 8 d 8 8 8 8 8 8 d 8 f . 
+            . . . f 8 6 d 8 8 8 8 d 6 8 f . 
+            . . . f f 8 8 8 8 8 8 8 8 f f . 
+            . . . . f f . . . . . . f f . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingUp, Predicate.MovingLeft, Predicate.MovingRight)
+        )
+        characterAnimations.loopFrames(
+        CopCar,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . 6 6 6 6 6 6 6 6 . . . . 
+            . . . 6 9 6 6 6 6 6 6 c 6 . . . 
+            . . 6 c 9 6 6 6 6 6 6 c c 6 . . 
+            . 6 c c 9 9 9 9 9 9 6 c c 9 6 d 
+            . 6 c 6 8 8 8 8 8 8 8 b c 9 6 6 
+            . 6 6 8 b b 8 b b b 8 8 b 9 6 6 
+            . 6 8 b b b 8 b b b b 8 6 6 6 6 
+            . 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6 
+            . 8 8 8 8 8 8 f 8 8 8 f 8 6 d d 
+            . 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d 
+            . 8 8 8 8 8 8 f f f 8 8 8 8 8 8 
+            . 8 f f f f 8 8 8 8 f f f 8 8 8 
+            . . f f f f f 8 8 f f f f f 8 . 
+            . . . f f f . . . . f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingRight, Predicate.MovingDown, Predicate.MovingUp)
+        )
+        characterAnimations.loopFrames(
+        CopCar,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 6 6 6 6 6 6 . . . . 
+            . . . . . 6 6 9 9 6 6 6 6 . . . 
+            . . . . . c 9 6 6 6 6 6 c . . . 
+            . . . . 6 c 9 6 6 6 6 6 c 6 . . 
+            . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
+            . . . f 6 c 9 6 6 6 6 6 c 6 f . 
+            . . . f 8 c 6 6 6 6 6 6 c 8 f . 
+            . . . f 6 c 6 b b b b 6 c 6 f . 
+            . . . 8 6 6 b c c c c b 6 6 8 . 
+            . . . 8 8 b c c c c c c b 8 8 . 
+            . . . f 8 9 9 9 9 9 9 9 9 8 f . 
+            . . . f 8 d 6 6 6 6 6 6 d 8 f . 
+            . . . . 6 d d 6 6 6 6 d d 6 f . 
+            . . . . f 6 d 6 6 6 6 d 6 f . . 
+            . . . . . 8 6 6 6 6 6 6 8 . . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingDown, Predicate.MovingLeft, Predicate.MovingRight)
+        )
+        characterAnimations.loopFrames(
+        CopCar,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 6 6 6 6 6 6 6 6 . . 
+            . . . . . 6 c 6 6 6 6 6 6 9 6 . 
+            . . . . 6 c c 6 6 6 6 6 6 9 c 6 
+            . . d 6 9 c c 6 9 9 9 9 9 9 c c 
+            . d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
+            . 6 6 6 9 b 8 8 b b b 8 b b 8 6 
+            . 6 6 6 6 6 8 b b b b 8 b b b 8 
+            . 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
+            . 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
+            . d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
+            . 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
+            . 8 8 8 8 f f f 8 8 8 8 f f f f 
+            . . . 8 f f f f f 8 8 f f f f f 
+            . . . . f f f f . . . . f f f . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingLeft, Predicate.MovingDown, Predicate.MovingUp)
+        )
         for (let value of tiles.getTilesByType(assets.tile`myTile18`)) {
             tiles.setTileAt(value, sprites.vehicle.roadHorizontal)
         }
@@ -466,7 +551,7 @@ function SetCopCar () {
             . . . . f f . . . . . . f f . . 
             `],
         500,
-        characterAnimations.rule(Predicate.MovingUp, Predicate.MovingDown, Predicate.MovingUp)
+        characterAnimations.rule(Predicate.MovingUp, Predicate.MovingLeft, Predicate.MovingRight)
         )
         characterAnimations.loopFrames(
         CopCar,
@@ -512,7 +597,7 @@ function SetCopCar () {
             . . . . . 8 6 6 6 6 6 6 8 . . . 
             `],
         500,
-        characterAnimations.rule(Predicate.MovingDown, Predicate.MovingDown, Predicate.MovingUp)
+        characterAnimations.rule(Predicate.MovingDown, Predicate.MovingLeft, Predicate.MovingRight)
         )
         characterAnimations.loopFrames(
         CopCar,
@@ -563,6 +648,98 @@ function SpawnMainCar () {
         scene.cameraFollowSprite(MainCar)
         tiles.setTileAt(value, sprites.vehicle.roadVertical)
         controller.moveSprite(MainCar)
+        characterAnimations.loopFrames(
+        MainCar,
+        [img`
+            . . . . . . e e c c e e . . . . 
+            . . . . . e 2 2 2 2 2 2 e . . . 
+            . . . . 2 c 2 2 2 2 2 2 c 2 . . 
+            . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+            . . . f 2 2 4 2 2 2 2 2 c 2 f . 
+            . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+            . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+            . . . f 2 c 2 4 4 2 2 2 c 2 f . 
+            . . . e 2 c e c c c c e c 2 e . 
+            . . . e 2 e c b b b b c e 2 e . 
+            . . . e 2 e b b b b b b e 2 e . 
+            . . . e e e e e e e e e e e e . 
+            . . . f e d e e e e e e d e f . 
+            . . . f e 2 d e e e e d 2 e f . 
+            . . . f f e e e e e e e e f f . 
+            . . . . f f . . . . . . f f . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingUp, Predicate.MovingLeft, Predicate.MovingRight)
+        )
+        characterAnimations.loopFrames(
+        MainCar,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . 2 2 2 2 2 2 2 2 . . . . 
+            . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+            . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+            . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+            . 2 c 2 e e e e e e e b c 4 2 2 
+            . 2 2 e b b e b b b e e b 4 2 2 
+            . 2 e b b b e b b b b e 2 2 2 2 
+            . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+            . e e e e e e f e e e f e 2 d d 
+            . e e e e e e f e e f e e e 2 d 
+            . e e e e e e f f f e e e e e e 
+            . e f f f f e e e e f f f e e e 
+            . . f f f f f e e f f f f f e . 
+            . . . f f f . . . . f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingRight, Predicate.MovingDown, Predicate.MovingUp)
+        )
+        characterAnimations.loopFrames(
+        MainCar,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 2 2 2 2 2 2 2 2 . . 
+            . . . . . 2 c 2 2 2 2 2 2 4 2 . 
+            . . . . 2 c c 2 2 2 2 2 2 4 c 2 
+            . . d 2 4 c c 2 4 4 4 4 4 4 c c 
+            . d 2 2 4 c b e e e e e e e 2 c 
+            . 2 2 2 4 b e e b b b e b b e 2 
+            . 2 2 2 2 2 e b b b b e b b b e 
+            . 2 2 2 2 e 2 2 2 2 2 e 2 2 2 e 
+            . 2 d d 2 e f e e e f e e e e e 
+            . d d 2 e e e f e e f e e e e e 
+            . e e e e e e e f f f e e e e e 
+            . e e e e f f f e e e e f f f f 
+            . . . e f f f f f e e f f f f f 
+            . . . . f f f f . . . . f f f . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingLeft, Predicate.MovingDown, Predicate.MovingUp)
+        )
+        characterAnimations.loopFrames(
+        MainCar,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 2 2 2 2 2 2 . . . . 
+            . . . . . 2 2 4 4 2 2 2 2 . . . 
+            . . . . . c 4 2 2 2 2 2 c . . . 
+            . . . . 2 c 4 2 2 2 2 2 c 2 . . 
+            . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+            . . . f 2 c 4 2 2 2 2 2 c 2 f . 
+            . . . f e c 2 2 2 2 2 2 c e f . 
+            . . . f 2 c 2 b b b b 2 c 2 f . 
+            . . . e 2 2 b c c c c b 2 2 e . 
+            . . . e e b c c c c c c b e e . 
+            . . . f e 4 4 4 4 4 4 4 4 e f . 
+            . . . f e d 2 2 2 2 2 2 d e f . 
+            . . . . 2 d d 2 2 2 2 d d 2 f . 
+            . . . . f 2 d 2 2 2 2 d 2 f . . 
+            . . . . . e 2 2 2 2 2 2 e . . . 
+            `],
+        500,
+        characterAnimations.rule(Predicate.MovingDown, Predicate.MovingLeft, Predicate.MovingRight)
+        )
     }
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile30`, function (sprite, location) {
